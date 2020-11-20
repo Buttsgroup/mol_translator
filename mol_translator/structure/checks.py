61fd55d1318093077c31c83a213127a4cfc90bbf
Calvin Yiu <chunnyyiu@hotmail.com>
2020-11-20 15:39:34 +0000

    initial checks written- not tested

--- mol_translator/structure/checks.py
+++ mol_translator/structure/checks.py
@@ -15,52 +15,67 @@
 #along with autoenrich.  If not, see <https://www.gnu.org/licenses/>.
 from mol_translator.aemol import aemol
 from mol_translator.structure.find_num_bonds import rdmol_find_num_bonds
+from rdkit import Chem.rdMolTransforms as Chem
 
 def run_all_checks(aemol):
 
-    check1 = check_valence(aemol)
-    check2 = check_missing_H(aemol)
-    check3 = check_bonds_are_plausible(aemol)
-
-    check = check1 and check2 and check3
-
-    return check
-
+    if not check_valence(aemol):
+        return False
+    if not check_missing_H(aemol):
+        return False
+    if not check_bonds_are_plausible(aemol):
+        return False
+    return True
 
 def check_valence(aemol):
-    check = True
-
-    # check valences are roughly correct
 
     rdmol = aemol.to_rdkit()
     num_bond_dict = rdmol_find_num_bonds(rdmol)
     for num_bonds in num_bond_dict.values():
         if len(num_bonds)>1:
-            check = False
-            print(f"Unexpected number of valence electrons in mol, {aemol.info['molid']}")
-        else continue
-    return check
+            return False
+            #print(f"Unexpected number of valence electrons in mol, {aemol.info['molid']}")
+        else:
+            continue
+    return True
 
 def check_missing_H(aemol):
-    check = True
-
     # everything should have hydrogens in it
 
     if aemol.structure['types']['H'] == 0:
-        print(f"No Hs in mol, {aemol.info['molid']}")
+        return False
+        #print(f"No Hs in mol, {aemol.info['molid']}")
+    else:
+        return True
 
-    return check
+def check_bonds_are_plausible_and_atom_overlap(aemol):
 
-def check_bonds_are_plausible(aemol):
-    check = True
+    rdmol = aemol.to_rdkit()
+    for i_idx, i_atom in enumerate(rdmol.GetAtoms()):
+        for j_idx, j_atom in enumerate(rdmol.GetAtoms()):
+            if i_idx == j_idx:
+                continue
+            bond = rdmol.GetBondBetweenAtoms(i_idx, j_idx)
+            if bond is not None:
+                conf = rdmol.GetConformer()
+                bond_length = Chem.GetBondLength(conf, i_idx, j_idx)
+                if bond_length >= 0.90 and bond_length <= 2.60:
+                    continue
+                else:
+                    return False
+            else:
+                i_atom_pos = [rdmol.GetConformer().GetAtomPosition(i_idx).x,
+                              rdmol.GetConformer().GetAtomPosition(i_idx).y,
+                              rdmol.GetConformer().GetAtomPosition(i_idx).z]
 
-    plausible_bonds = {"1": [MIN_VALUE, MAX_VALUE]}
-    .
-    .
-    .
-
-    # is every bond within a plausible distance range
+                j_atom_pos = [rdmol.GetConformer().GetAtomPosition(j_idx).x,
+                              rdmol.GetConformer().GetAtomPosition(j_idx).y,
+                              rdmol.GetConformer().GetAtomPosition(j_idx).z]
+                average_dist = np.linalg.norm(i_atom_pos - j_atom_pos)
+                if average_dist < 1.0:
+                    return False
 
     return True
 
-def check_atoms_dont_overlap(aemol):
+    #angstrom check for possible bond lengths, likely with dictionary values, account for bond order
+    # is every bond within a plausible distance range
