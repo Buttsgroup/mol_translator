ee70cfef701e9f05581cf3ba18d321d1b17075a6
Calvin Yiu <chunnyyiu@hotmail.com>
2022-06-16 13:39:21 +0100

    rtd conf.py path change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../mol_translator'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
